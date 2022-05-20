a88475e34b334e893d055867576f09731845f504
joseIgnaciodv <joseignaciodelvallebustillo@gmail.com>
2022-05-20 16:16:47 +0200

    conflicto con endpoints resuelto

--- src/app/componentes/localidad/localidad.component.ts
+++ src/app/componentes/localidad/localidad.component.ts
@@ -31,13 +31,13 @@ export class LocalidadComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.api.get_localidad(this.localidad).subscribe(respuesta =>{
-        this.num_odio = respuesta.odio;
-      });}, 200);
     this.localidad = this.route.snapshot.params['localidad'];
     this.tipo = this.route.snapshot.params['tipo']
-    this.get_inmuebles();
+    this.api.get_localidad(this.localidad).subscribe(respuesta =>{
+      this.num_odio = respuesta.odio;
+      this.get_inmuebles();
+    })
+
   }
 
 }
