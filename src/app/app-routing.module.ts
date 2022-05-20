143d5be32c571f81a505d507039190f83d06fa9b
Ricardo <elrichardev@gmail.com>
2022-05-20 10:41:50 +0200

    Añadido sobre-nosotros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { LocalidadComponent } from './componentes/localidad/localidad.component';
+import { SobreNosotrosComponent } from './componentes/sobre-nosotros/sobre-nosotros.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
-  {path: "localidad/:localidad/:tipo", component: LocalidadComponent}
+  {path: "localidad/:localidad/:tipo", component: LocalidadComponent},
+  {path: "sobre-nosotros", component: SobreNosotrosComponent}
 ];
 
 @NgModule({
