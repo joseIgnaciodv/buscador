150d2362fa57ed20dd65a47376b487e531936886
joseIgnaciodv <joseignaciodelvallebustillo@gmail.com>
2022-05-20 14:53:16 +0200

    rutas en el modulo de enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,8 @@ import { SobreNosotrosComponent } from './componentes/sobre-nosotros/sobre-nosot
 const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: "localidad/:localidad/:tipo", component: LocalidadComponent},
-<<<<<<< HEAD
-  {path: "inmuebles/:id", component: InmuebleComponent}
-=======
+  {path: "inmuebles/:id", component: InmuebleComponent},
   {path: "sobre-nosotros", component: SobreNosotrosComponent}
->>>>>>> 143d5be32c571f81a505d507039190f83d06fa9b
 ];
 
 @NgModule({
