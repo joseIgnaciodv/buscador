4babe816e1cc6a60ce04b100d4f845ea16c633cf
Ricardo <elrichardev@gmail.com>
2022-05-26 17:00:27 +0200

    Creada interfaz lugar_interes y nuevo metodo get_lugares_interes en el servicio de la api. Pendiente de subir nueva vista de inmueble y ts actualizado.

---
+++ src/app/modelos/lugar_interes.ts
@@ -0,0 +1,15 @@
+export interface LugarInteres {
+    'id': number,
+    'created_at': Date,
+    'updated_at': Date,
+    'nombre': string,
+    'direccion': string,
+    'latitud':number,
+    'longitud':number,
+    'longitudRadius':number,
+    'latitudRadius':number,
+    'tipo_establecimiento':string
+    'telefono': string,
+    'puntuacion_media': number,
+    'media_analisis': string
+}
--- src/app/servicios/api.service.ts
+++ src/app/servicios/api.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { Localidad } from '../modelos/localidad';
 import { environment } from 'src/environments/environment';
 import { Inmueble } from '../modelos/inmueble';
+import { LugarInteres } from '../modelos/lugar_interes';
 import { Usuario } from '../modelos/usuario';
 
 
@@ -27,6 +28,11 @@ export class ApiService {
   get_inmueble_concreto(id: number): Observable<Inmueble>{
     let url = environment.apiUrl + "api/inmuebles/" + id;
     return this.http.get<Inmueble>(url);
+  }
+
+  get_lugares_interes(latitud: string, longitud: string): Observable<Array<LugarInteres>>{
+    let url = environment.apiUrl + "api/lugar_interes/" + latitud + "/" + longitud;
+    return this.http.get<Array<LugarInteres>>(url);
   }
 
   login(email: string, password: string): Observable<Usuario>{
